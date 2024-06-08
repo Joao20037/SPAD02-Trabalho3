4ef36d9e1c21023a52f8663521b14fd4b76596c8
Joao20037 <joaolucas.moraes.oliveira@gmail.com>
2024-06-08 20:38:19 -0300

    dao progress

--- daoORM.py
+++ daoORM.py
@@ -1,2 +1,21 @@
+import sqlalchemy
+
 class Dao:
-    ...
+    
+    def __init__(self, dbname, user, password, host="localhost", port=5432):
+
+        self.engine = sqlalchemy.create_engine(f"postegresql+psycopg://{user}:{password}@{host}:{port}/{dbname}")
+
+
+    def insert_order(self, model_order):
+
+        Session = sqlalchemy.sessionmaker()
+        session = Session()
+
+        session.add(model_order)
+
+        session.commit()
+
+        order = session.query(model_order)
+
+        return order.id    
